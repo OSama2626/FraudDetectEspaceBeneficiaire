712faf944909b2534936ad578d4e9e08700d1d4f
Somi <zineb.baqili@enim.ac.ma>
2025-11-11 12:56:29 +0100

    feat: auth 2

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,30 +1,16 @@
 # backend/app/models/user.py
-from sqlalchemy import Column, String, Integer
-
-# --- NOUVEAUX IMPORTS ---
+from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from typing import Optional
-# -------------------------
-
-# Importer 'Base' depuis son nouvel emplacement
 from ..core.db import Base 
 
 class User(Base):
-    """
-    Modèle SQLAlchemy (style 2.0) pour la table 'users'.
-    """
     __tablename__ = "users"
 
-    # --- NOUVELLE SYNTAXE ---
-    # Utiliser Mapped et mapped_column
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    
     clerk_id: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-    
-    # Rendre les champs optionnels avec Optional[]
     email: Mapped[Optional[str]] = mapped_column(String, unique=True, index=True)
     first_name: Mapped[Optional[str]] = mapped_column(String)
     last_name: Mapped[Optional[str]] = mapped_column(String)
     image_url: Mapped[Optional[str]] = mapped_column(String)
-    # -------------------------
